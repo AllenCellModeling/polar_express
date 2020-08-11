fb5ea9c3e3df1d178b174ecbca4aa6708bf4b4ec
William Chen <william.chen@dev-aics-gjp-002.corp.alleninstitute.org>
2020-08-11 01:17:13 -0700

    Update test_computeVoxelMatrix

--- polar_express/tests/test_computeVoxelMatrix.py
+++ polar_express/tests/test_computeVoxelMatrix.py
@@ -8,6 +8,6 @@ import numpy as np
 
 def testVoxelMatricesEqual(test_image_metrics, true_image_metrics):
 
-    assert np.alltrue(
-        test_image_metrics["voxel_matrix"] == true_image_metrics["voxel_matrix"]
+    assert np.array_equal(
+        test_image_metrics["voxel_matrix"], true_image_metrics["voxel_matrix"]
     )

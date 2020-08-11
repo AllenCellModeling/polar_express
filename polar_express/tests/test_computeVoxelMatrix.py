d194a73fc24d9b502bb323932d0cac58de548dab
William Chen <william.chen@dev-aics-gjp-002.corp.alleninstitute.org>
2020-08-11 00:57:52 -0700

    Changed test names to be unique

--- polar_express/tests/test_computeVoxelMatrix.py
+++ polar_express/tests/test_computeVoxelMatrix.py
@@ -6,7 +6,7 @@ import numpy as np
 #######################################################################################
 
 
-def testMatricesEqual(test_image_metrics, true_image_metrics):
+def testVoxelMatricesEqual(test_image_metrics, true_image_metrics):
 
     assert np.alltrue(
         test_image_metrics["voxel_matrix"] == true_image_metrics["voxel_matrix"]
--- polar_express/tests/test_findVerticalCutoffs.py
+++ polar_express/tests/test_findVerticalCutoffs.py
@@ -5,7 +5,7 @@
 #######################################################################################
 
 
-def testMatricesEqual(test_image_metrics, true_image_metrics):
+def testVerticalCutoffsEqual(test_image_metrics, true_image_metrics):
 
     assert test_image_metrics["min_z_dna"] == true_image_metrics["min_z_dna"]
     assert test_image_metrics["max_z_dna"] == true_image_metrics["max_z_dna"]
